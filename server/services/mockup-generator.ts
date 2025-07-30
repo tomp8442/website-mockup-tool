28f144bf41e439460f728845e52ad238b234dbac
tomp8442 <tomp4479@gmail.com>
2025-07-29 21:07:20 -0400

    Update mockup-generator.ts

--- server/services/mockup-generator.ts
+++ server/services/mockup-generator.ts
@@ -23,7 +23,6 @@ export class MockupGenerator {
     
     this.browser = await puppeteer.launch({
       headless: true,
-      executablePath: '/nix/store/zi4f80l169xlmivz8vja8wlphq74qqk0-chromium-125.0.6422.141/bin/chromium',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
